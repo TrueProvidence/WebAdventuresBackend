bec198f8831b3ef33288835c0c5db7cc353967f9
Lucarius13 <luca@inkblu.nl>
2024-09-23 10:13:37 +0200

    Made Characters into a navigatable page
    Used to be you needed to know the URL itself to find it. Now it has a button :D

--- DotnetMVC/obj/Debug/net8.0/DotnetMVC.AssemblyInfo.cs
+++ DotnetMVC/obj/Debug/net8.0/DotnetMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a73bb3ba5db0ecc5b4955fececce67e88c317e4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d9a677d9a313c4e48fbd56fad84773c7872981")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
